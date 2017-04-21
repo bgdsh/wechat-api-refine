1fec7335055d1ba2ae587fcf8bd2234fe4e6eb26
bgd_sh <bgd_sh@hotmail.com>
2017-04-21 14:18:12 +0800

    ignore compiled file

--- lib/ZipCodeValidator.d.ts
+++
@@ -1,5 +0,0 @@
-import { StringValidator } from './Validation';
-export declare const numberRegexp: RegExp;
-export declare class ZipCodeValidator implements StringValidator {
-    isAcceptable(s: string): boolean;
-}
